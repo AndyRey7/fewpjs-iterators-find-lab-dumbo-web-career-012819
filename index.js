cafd150466f103625b00d9e1f2233a39954eab8e
Andy Reyes <11710reyes@gmail.com>
2019-09-21 17:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const record = [
 
 const superbowlWin = (record) => {
   let win = record.find(r => r.result === "W")
-  if (win === "W") {
+  if (win.result === "W") {
     return win.year
   } else {
     undefined
